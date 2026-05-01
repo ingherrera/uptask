feadf53ffd95e382963f5615a565c1fc4964a549
Pascual <ing_herrera16@hotmail.com>
2026-04-30 18:34:51 -0600

    Del contenedor a Git

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,39 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite"; 
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  clearScreen: false,
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    host: true,
-    port: 5174,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // Tipamos 'id' como string para evitar el error de 'any' implícito
-        manualChunks(id: string) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
+export default defineConfig(({ mode }) => {
+  // Lee las ENV que empiezan con VITE_ 
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    clearScreen: false,
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-    chunkSizeWarningLimit: 800,
-  },
+    server: {
+      host: true,
+      port: 5174,
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: string) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+        },
+      },
+      chunkSizeWarningLimit: 800,
+    },
+    // Esto fuerza el reemplazo de la variable en el build
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+    }
+  }
 });
